b3a198b37edce64786f659ea6704c267f54f23c5
Youssef Ahmed <84088109+cstfre@users.noreply.github.com>
2024-11-07 15:08:43 +0200

    Update SignIn.jsx

--- src/Components/Auth/SignIn.jsx
+++ src/Components/Auth/SignIn.jsx
@@ -75,7 +75,7 @@ const SignIn = () => {
         <div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
           <div className="w-full h-100">
             <img
-              src="/Assets/Extra-Assets/Quote1.jpg"
+              src="/Assets/Extra-Assets/Logo.png"
               alt="logo"
               width={150}
               height={200}
